711f76b9b6e1a80cdd0f2b0dcbdd3491c9ae5f63
dustyfingers <dustyfingers01257@gmail.com>
2019-07-04 20:09:21 -0600

    added aux wrapper component, added layout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div>
+            <Layout>
+                <p>test content!</p>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
